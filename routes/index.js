00a8a4e8df17bc4426d581aee920399d1e4ba1d8
HanmaDaiki <daikihanma@outlook.com>
2023-05-16 20:23:44 +0300

    fix routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,11 @@
 const router = require('express').Router();
-const { celebrate, Joi } = require('celebrate');
-
 const { signUp, signIn } = require('../controllers/sign');
 const auth = require('../middlewares/auth');
 const users = require('./users');
-const NotFoundError = require('../errors/NotFoudError');
 
-router.post('/signup', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().email().required(),
-    password: Joi.string().required(),
-    name: Joi.string().required(),
-  }),
-}), signUp);
-router.post('/signin', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().email().required(),
-    password: Joi.string().required(),
-  }),
-}), signIn);
+router.post('/signup', signUp);
+router.post('/signin', signIn);
 
 router.use('/users', auth, users);
-
-router.use(auth, (req, res, next) => {
-  next(new NotFoundError('Page Not Found'));
-});
 
 module.exports = router;
